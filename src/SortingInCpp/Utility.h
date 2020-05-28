59b92259a2c52a5d907268bf9c56a619f655fdc2
Rathnavel Sankaralingam <pixrat92@gmail.com>
2020-05-28 11:45:12 +0530

    Adding Time Calculations

---
+++ src/SortingInCpp/Utility.h
@@ -0,0 +1,65 @@
+//Rathinavel Sankaralingam
+//May 26, 2020
+
+
+#include <iostream>
+#include <vector>
+#include <chrono>
+#include <sstream>
+
+#pragma once
+
+using namespace std;
+
+// Defines
+#define LINE_RETURN cout << endl;
+
+// Typedefs
+typedef   chrono::steady_clock::time_point        STEADY_CLOCK_TIMEPOINT;
+typedef   chrono::duration<double>                DURATION;
+
+// Global Variables
+STEADY_CLOCK_TIMEPOINT start;
+STEADY_CLOCK_TIMEPOINT finish;
+DURATION duration;
+
+template<typename T>
+void print(T &str, vector<int> &arr)
+{
+  cout << str << endl;
+
+  for(int a : arr)
+    cout << a << "  ";
+
+  LINE_RETURN
+}
+
+template<typename T>
+void print(T str)
+{
+  cout << str;
+}
+
+template<typename T1, typename T2>
+void print(T1 str1, T2 str2)
+{
+  cout << str1 << str2;
+}
+
+void startClock()
+{
+  start  = std::chrono::steady_clock::now();
+}
+
+void endClock()
+{
+  finish = std::chrono::steady_clock::now();
+}
+
+void printDuration()
+{
+  duration = finish - start;  
+
+  print("\n", duration.count() * 1000);
+  print(" milliseconds\n");
+}
--- src/SortingInCpp/main.cpp
+++ src/SortingInCpp/main.cpp
@@ -4,8 +4,9 @@
 #include <iostream>
 #include <vector>
 
-#include "Utility.cpp"
+#include "Utility.h"
 
+#pragma once
 using namespace std;
 
 #define SORTED    "\nThe sorted array is:"
@@ -13,11 +14,10 @@ using namespace std;
 
 void runQuicksort(vector<int> &arr);
 
-
 void loadArray(vector<int> &arr)
 {
   arr = {};
-  arr = {45,32,52,266,18,92,256,76,23,54,89};
+  arr = {45,32,52,266,18,92,256,76,23,54,89,182,90,321,43,547,209,121,34,522,450};
 }
 
 void main()
@@ -25,10 +25,16 @@ void main()
   vector<int> arr;
 
   loadArray(arr);
-  print(SORTED,arr);
+  print(UNSORTED,arr);
 
+  startClock();
   runQuicksort(arr);
-  print(UNSORTED, arr);
+  endClock();
 
+  LINE_RETURN
+  print("\nUsing Quicksort");
+  print(SORTED, arr);
+  printDuration();
+  
   getchar();
 }
